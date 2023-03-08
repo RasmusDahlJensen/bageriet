6191037358f7452f3d36e8e820c37d3485ba0bf3
Rasmus Jensen <rasmusdj96@gmail.com>
2023-03-08 14:44:37 +0100

    Update ProductDetails.jsx

--- src/Pages/ProductDetails.jsx
+++ src/Pages/ProductDetails.jsx
@@ -11,7 +11,7 @@ export const ProductDetails = () => {
 	const [product, getProduct] = useState([]);
 	useEffect(() => {
 		axios.get(url).then((data) => {
-			// console.log(data.data);
+			console.log(data.data);
 			getProduct(data.data.item);
 			setLoading(false);
 		});
